b75db991e0e6debe779c5ba0977dfb4b729e9105
Angela Vawser <amvawser@gmail.com>
2019-08-17 15:12:57 -0700

    final touches to css

--- src/components/options/Search.js
+++ src/components/options/Search.js
@@ -38,13 +38,13 @@ class Search extends Component {
                 <div class="search-name">
                     <h2>Search By Name:</h2>
                     <label>
-                        <input name="search" value="${search}" type="text">
+                        <input class="name-input" name="search" value="${search}" type="text">
                     </label>
                 </div>
 
                 <div class="filter-by">
                     <h2>Filter By:</h2>
-                    <select name="filter-param">
+                    <select class="filter-param" name="filter-param">
                         <option value="type" type="text" ${this.selectedFilterParam(selectedFilterParam, 'type')}>TYPE</option>
                         <option value="eggGroup" type="text" ${this.selectedFilterParam(selectedFilterParam, 'eggRoup')}>EGG GROUP</option>
                         <option value="ability" type="text" ${this.selectedFilterParam(selectedFilterParam, 'ability')}>ABILITY</option>
